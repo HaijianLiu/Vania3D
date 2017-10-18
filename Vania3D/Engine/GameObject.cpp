2d98c47b1f1f6b848b69472c508c955aa06491a6
HaijianLiu <haijanliu@gmail.com>
2017-10-18 11:49:16 +0900

    chore

--- Vania3D/Engine/GameObject.cpp
+++ Vania3D/Engine/GameObject.cpp
@@ -21,7 +21,7 @@ GameObject::~GameObject() {
 < start >
 ------------------------------------------------------------------------------*/
 void GameObject::start() {
-	for (const auto &conponent : this->components) {
+	for (const auto & conponent : this->components) {
 		conponent.second->start();
 	}
 }
@@ -31,7 +31,7 @@ void GameObject::start() {
 < update >
 ------------------------------------------------------------------------------*/
 void GameObject::update() {
-	for (const auto &conponent : this->components) {
+	for (const auto & conponent : this->components) {
 		conponent.second->update();
 	}
 }
