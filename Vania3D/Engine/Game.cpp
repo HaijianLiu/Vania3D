6b8a4dc37da36c8e73cd458463411bfd6f13ac8f
HaijianLiu <haijanliu@gmail.com>
2017-11-06 12:21:31 +0900

    add camera and player to a new scene

--- Vania3D/Engine/Game.cpp
+++ Vania3D/Engine/Game.cpp
@@ -5,7 +5,7 @@
 < Constructor >
 ------------------------------------------------------------------------------*/
 Game::Game() {
-	
+
 }
 
 Game* Game::getInstance() {
@@ -43,10 +43,12 @@ void Game::start() {
 	// resources
 	this->resources->start();
 	// render pass
-	this->renderPass->init(this->resources->getShader("renderPass"), 5);
+	this->renderPass->init(this->resources->getShader("renderPass"), 5); // 3 model textures + 1 model position + 1 alpha
+
 	// scene manager ????
-	this->sceneManager->addScene("Scene00", new Scene00()); // default scene00
-	this->sceneManager->setActiveScene("Scene00");
+	this->sceneManager->addScene("Scene00", new Scene00());
+	this->sceneManager->addScene("Scene01", new Scene01());
+	this->sceneManager->setActiveScene("Scene01"); // set default scene
 }
 
 
--- Vania3D/Scene/Scene01.cpp
+++ Vania3D/Scene/Scene01.cpp
@@ -21,7 +21,50 @@ Scene01::~Scene01() {
 < start >
 ------------------------------------------------------------------------------*/
 void Scene01::start() {
+	Game* game = Game::getInstance();
 
+	// camera
+	GameObject* camera = new GameObject();
+	Transform* cameraTransform = camera->addComponent<Transform>();
+	cameraTransform->position = glm::vec3(0.0,10.0,20.0);
+	camera->addComponent<Camera>();
+
+	// player
+	GameObject* player = new GameObject();
+	Transform* playerTransform = player->addComponent<Transform>();
+	playerTransform->modelScale = glm::vec3(0.05);
+	PlayerController* playerController = player->addComponent<PlayerController>();
+	playerController->camera = camera;
+	CameraController* cameraController = player->addComponent<CameraController>();
+	cameraController->camera = camera;
+	MeshRenderer* playerMeshRenderer = player->addComponent<MeshRenderer>();
+	playerMeshRenderer->addModel(game->resources->getModel("player"));
+	playerMeshRenderer->addMaterial(game->resources->getMaterial("player"));
+	playerMeshRenderer->addLightProbe(game->resources->getLightProbe("hdr"));
+	playerMeshRenderer->camera = camera;
+	this->addGameObject("player", player);
+
+	// camera target
+	GameObject* cameraTarget = new GameObject();
+	Transform* cameraTargetTransform = cameraTarget->addComponent<Transform>();
+	Offset* cameraTargetOffset = cameraTarget->addComponent<Offset>();
+	cameraTargetOffset->parent = playerTransform;
+	cameraTargetOffset->offsetPosition = glm::vec3(0, 5, 0);
+	this->addGameObject("cameraTarget", cameraTarget);
+
+	// camera
+	camera->getComponent<Camera>()->target = cameraTargetTransform;
+	camera->getComponent<Camera>()->offsetFromTarget = cameraTransform->position - camera->getComponent<Camera>()->target->position;
+	this->addCamera(camera);
+
+
+
+	// IBL
+	game->renderPass->setActiveLightProbe(game->resources->getLightProbe("hdr"));
+
+	// Enable alpha channel after generate prefilter map
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
 }
 
 
