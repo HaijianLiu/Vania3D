3ddb1a7fe291e317ffab7c29ec4fc74826d01c4a
HaijianLiu <haijanliu@gmail.com>
2018-03-28 23:15:21 +0900

    test outline shader

--- Assets/Shaders/RenderPass/renderpass_outline_1_passes.fs.glsl
+++ Assets/Shaders/RenderPass/renderpass_outline_1_passes.fs.glsl
@@ -12,5 +12,36 @@ void main() {
 	vec3 n = texture(normalPass, uv).rgb;
 	vec3 position = texture(positionPass, uv).rgb;
 
-	outlinePass = vec4(position, 1.0);
+	float dx = 1.0 / 1920.0;
+  float dy = 1.0 / 1080;
+
+  vec3 center = texture(normalPass, uv).rgb;
+
+  vec3 top = texture(normalPass, uv + vec2(0, dy)).rgb;
+  vec3 topRight = texture(normalPass, uv + vec2(dx, dy)).rgb;
+  vec3 right = texture(normalPass, uv + vec2(dx, 0)).rgb;
+
+  vec3 t = center - top;
+  vec3 r = center - right;
+  vec3 tr = center - topRight;
+
+  t = abs( t );
+  r = abs( r );
+  tr = abs( tr );
+
+  float samp;
+  samp = max( samp, t.x );
+  samp = max( samp, t.y );
+  samp = max( samp, t.z );
+  samp = max( samp, r.x );
+  samp = max( samp, r.y );
+  samp = max( samp, r.z );
+  samp = max( samp, tr.x );
+  samp = max( samp, tr.y );
+  samp = max( samp, tr.z );
+
+	samp = clamp(0.0, 1.0, samp);
+	samp = 1.0 - samp;
+
+	outlinePass = vec4(vec3(samp), 1.0);
 }
