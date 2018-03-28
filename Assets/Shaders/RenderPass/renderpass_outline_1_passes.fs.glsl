2358ca427f14aa0c5ab00bba60bf58d161683036
HaijianLiu <haijanliu@gmail.com>
2018-03-28 23:25:05 +0900

    fix some syntax

--- Assets/Shaders/RenderPass/renderpass_outline_1_passes.fs.glsl
+++ Assets/Shaders/RenderPass/renderpass_outline_1_passes.fs.glsl
@@ -13,32 +13,32 @@ void main() {
 	vec3 position = texture(positionPass, uv).rgb;
 
 	float dx = 1.0 / 1920.0;
-  float dy = 1.0 / 1080;
+	float dy = 1.0 / 1080;
 
-  vec3 center = texture(normalPass, uv).rgb;
+	vec3 center = texture(normalPass, uv).rgb;
 
-  vec3 top = texture(normalPass, uv + vec2(0, dy)).rgb;
-  vec3 topRight = texture(normalPass, uv + vec2(dx, dy)).rgb;
-  vec3 right = texture(normalPass, uv + vec2(dx, 0)).rgb;
+	vec3 top = texture(normalPass, uv + vec2(0, dy)).rgb;
+	vec3 topRight = texture(normalPass, uv + vec2(dx, dy)).rgb;
+	vec3 right = texture(normalPass, uv + vec2(dx, 0)).rgb;
 
-  vec3 t = center - top;
-  vec3 r = center - right;
-  vec3 tr = center - topRight;
+	vec3 t = center - top;
+	vec3 r = center - right;
+	vec3 tr = center - topRight;
 
-  t = abs( t );
-  r = abs( r );
-  tr = abs( tr );
+	t = abs( t );
+	r = abs( r );
+	tr = abs( tr );
 
-  float samp;
-  samp = max( samp, t.x );
-  samp = max( samp, t.y );
-  samp = max( samp, t.z );
-  samp = max( samp, r.x );
-  samp = max( samp, r.y );
-  samp = max( samp, r.z );
-  samp = max( samp, tr.x );
-  samp = max( samp, tr.y );
-  samp = max( samp, tr.z );
+	float samp;
+	samp = max( samp, t.x );
+	samp = max( samp, t.y );
+	samp = max( samp, t.z );
+	samp = max( samp, r.x );
+	samp = max( samp, r.y );
+	samp = max( samp, r.z );
+	samp = max( samp, tr.x );
+	samp = max( samp, tr.y );
+	samp = max( samp, tr.z );
 
 	samp = clamp(0.0, 1.0, samp);
 	samp = 1.0 - samp;
